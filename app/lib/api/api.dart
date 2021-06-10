124a35e170813dc8e637e0e9a1557f109491de7c
roshanbhattarai98 <rsn.bhattarai@gmail.com>
2021-06-10 12:35:14 +0545

    loginapi

--- app/lib/api/api.dart
+++ app/lib/api/api.dart
@@ -5,10 +5,10 @@ import 'package:app/model/login_model.dart';
 
 class APIService {
   Future<LoginResponseModel> login(LogInRequestModel requestModel) async {
-    String url = "https://buytarkari.com/api/login";
+    String url = "http://buytarkari.sambatshree.com/api/login";
 
     final response = await http.post(
-        Uri.parse("https://buytarkari.com/api/login"),
+        Uri.parse("http://buytarkari.sambatshree.com/api/login"),
         body: requestModel.toJson());
     if (response.statusCode == 200 || response.statusCode == 400) {
       return LoginResponseModel.fromJson(
